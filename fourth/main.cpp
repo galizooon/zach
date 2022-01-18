936971649261dff93a5926f7d199a07ac4feb919
galizooon <semyon.belyaeff@yandex.ru>
2022-01-18 09:07:50 +0500

    New

---
+++ fourth/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 23 окт. 2021 г.
+ *      Author: Семен Беляев
+ */
+#include <iostream>
+#include <stdexcept>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
+#include "MazeWindow.h"
+int main (int, char**)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+try{
+ MazeWindow w;
+ w.main_loop();
+} catch(const std:: exception &e) {
+	std::cerr <<"error"<<e.what()<<std::endl;
+	return 1;
+} catch (...){
+	std::cerr<<"nameless error"<<std::endl;
+	return 1;
+}
+			return 0;
+}
+
---
+++ fourth/utils.h
@@ -0,0 +1,17 @@
+/*
+ * utils.h
+ *
+ *  Created on: 23 окт. 2021 г.
+ *      Author: Семен Беляев
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+#include <cmath>
+static constexpr double Pi=acos(-1.0);
+
+
+
+
+
+#endif /* UTILS_H_ */
